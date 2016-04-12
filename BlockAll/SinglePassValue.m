ce918baccd06360632cb74ac04cfcea0baab6f6d
qsyMac <qsy118614@126.com>
2016-04-13 07:04:18 +0800

    新增block的方法2

--- BlockAll/SinglePassValue.m
+++ BlockAll/SinglePassValue.m
@@ -9,7 +9,7 @@
 #import "SinglePassValue.h"
 
 @implementation SinglePassValue
-
+//方法1
 + (instancetype)shareInstance {
     static SinglePassValue *singleton = nil;
     static dispatch_once_t onceToken;
@@ -18,7 +18,17 @@
     });
     return singleton;
 }
-
+//方法2
+//+ (instancetype)shareInstance {
+//    static SinglePassValue *singleton = nil;
+////    资源加锁，保证线程单词只有1次被访问
+//    @synchronized(self) {
+//        if (!singleton) {
+//            singleton = [[SinglePassValue alloc]init];
+//        }
+//        return singleton;
+//    }
+//}
 - (instancetype)init {
     self = [super init];
     if (self) {
