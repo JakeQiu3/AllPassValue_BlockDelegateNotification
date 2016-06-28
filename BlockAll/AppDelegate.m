a956bba1cd824cec259627fb5c226ef541806e3e
JakeQiu3 <qsy118614@126.com>
2016-06-28 11:41:17 +0800

    hah

--- BlockAll/AppDelegate.m
+++ BlockAll/AppDelegate.m
@@ -17,7 +17,7 @@
 
 
 //
-//
+// hhh
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
