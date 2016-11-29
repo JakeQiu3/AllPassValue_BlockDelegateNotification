81fefa0882f7e16b2b224102dc38eb4887a70797
JakeQiu3 <qsy118614@163.com>
2016-11-29 15:42:47 +0800

    测试分支

--- BlockAll/AppDelegate.m
+++ BlockAll/AppDelegate.m
@@ -18,6 +18,8 @@
 
 // 4444
 // hhh
+
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
